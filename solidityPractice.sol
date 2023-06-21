630727c8df05df3e6bbea4221c92ce5b534d6d39
William <101371762+addicted2crypto@users.noreply.github.com>
2023-06-21 15:44:50 -0700

    daily code challenge for 06/21/2023
    scope practice for solidity

--- solidityPractice.sol
+++ solidityPractice.sol
@@ -1,3 +1,9 @@
 // .transfer() and .send() send 2300 gwei in gas..... so now we use this shit 
+//contract ScopePractice {
+
+  // Local variable
+  uint private localVariable = 10;
+
+  // State variable
 (bool success, ) = msg.sender.call.value(amount)("");
 require(success, "Transfer failed.");
